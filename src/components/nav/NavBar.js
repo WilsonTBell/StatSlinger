ed1288358e93f1bc1cc4e78e4fca9d7752768189
Wilson Bell <wilsontbell@gmail.com>
2022-07-22 12:47:28 -0500

    NavBar functioning

--- src/components/nav/NavBar.js
+++ src/components/nav/NavBar.js
@@ -1,3 +1,19 @@
+
+import { AdminNavBar } from "./AdminNavBar"
+import "./NavBar.css"
+import { ShooterNavBar } from "./ShooterNavBar"
+
+
 export const NavBar = () => {
-    return <></>
+
+
+    const localStatSlinger = localStorage.getItem("stat_slinger")
+    const  StatSlingerObject = JSON.parse(localStatSlinger)
+    
+    if(StatSlingerObject.admin) {
+        return <AdminNavBar/>
+    }
+    else {
+        return <ShooterNavBar/>
+    }  
 }
